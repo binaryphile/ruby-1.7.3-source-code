f85588b70ea9e242651656466fab8d8825d9d78c
Ted Lilley <ted.lilley@accelerated.com>
2017-04-19 23:39:23 -0400

    inits.c

--- inits.c
+++ inits.c
@@ -8,7 +8,7 @@
 
   Copyright (C) 1993-2002 Yukihiro Matsumoto
 
-**********************************************************************/
+ **********************************************************************/
 
 #include "ruby.h"
 
@@ -44,37 +44,37 @@ void Init_Time _((void));
 void Init_var_tables _((void));
 void Init_version _((void));
 
-void
+  void
 rb_call_inits()
 {
-    Init_sym();
-    Init_var_tables();
-    Init_Object();
-    Init_Comparable();
-    Init_Enumerable();
-    Init_Precision();
-    Init_eval();
-    Init_String();
-    Init_Exception();
-    Init_Thread();
-    Init_Numeric();
-    Init_Bignum();
-    Init_Array();
-    Init_Hash();
-    Init_Struct();
-    Init_Regexp();
-    Init_pack();
-    Init_Range();
-    Init_IO();
-    Init_Dir();
-    Init_Time();
-    Init_Random();
-    Init_signal();
-    Init_process();
-    Init_load();
-    Init_Proc();
-    Init_Math();
-    Init_GC();
-    Init_marshal();
-    Init_version();
+  Init_sym();
+  Init_var_tables();
+  Init_Object();
+  Init_Comparable();
+  Init_Enumerable();
+  Init_Precision();
+  Init_eval();
+  Init_String();
+  Init_Exception();
+  Init_Thread();
+  Init_Numeric();
+  Init_Bignum();
+  Init_Array();
+  Init_Hash();
+  Init_Struct();
+  Init_Regexp();
+  Init_pack();
+  Init_Range();
+  Init_IO();
+  Init_Dir();
+  Init_Time();
+  Init_Random();
+  Init_signal();
+  Init_process();
+  Init_load();
+  Init_Proc();
+  Init_Math();
+  Init_GC();
+  Init_marshal();
+  Init_version();
 }
